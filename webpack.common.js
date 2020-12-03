e36fc0d21075c0c9fb4bef8b6b8975f394c9e3fa
Ademu Anthony <ademuanthony@gmail.com>
2020-12-03 03:32:04 +0100

    Frontend setup

---
+++ webpack.common.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const StyleLintPlugin = require('stylelint-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './public/index.js'
+  },
+  externals: {
+    turbolinks: 'Turbolinks'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false, url: false, sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      // filename: '[name].css',
+      // chunkFilename: '[id].css'
+      filename: 'css/style.css'
+    }),
+    new StyleLintPlugin()
+  ],
+  output: {
+    filename: 'js/[name].bundle.js',
+    path: path.resolve(__dirname, 'public/dist'),
+    publicPath: '/dist/'
+  },
+  // Fixes weird issue with watch script. See
+  // https://github.com/webpack/webpack/issues/2297#issuecomment-289291324
+  watchOptions: {
+    poll: true
+  }
+}
