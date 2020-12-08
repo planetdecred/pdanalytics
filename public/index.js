74136fb4ce226f71b3a281c9dbb799118eb851d6
Ademu Anthony <ademuanthony@gmail.com>
2020-12-09 00:03:37 +0100

    Cleen the frontend

--- public/index.js
+++ public/index.js
@@ -1,11 +1,8 @@
 import 'regenerator-runtime/runtime'
 /* global require */
-// import ws from './js/services/messagesocket_service'
-import './js/services/desktop_notification_service'
 import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
 import { darkEnabled } from './js/services/theme_service'
-// import globalEventBus from './js/services/event_bus_service'
 
 require('./scss/application.scss')
 
