e128003adf1f3b303400ef4c7cd4cb8f81305e79
Ademu aNthony <ademuanthony@gmail.com>
2021-03-31 14:54:57 +0100

    Agenda page handler

--- gov/agendas/handlers.go
+++ gov/agendas/handlers.go
@@ -1,8 +1,11 @@
 package agendas
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/planetdecred/pdanalytics/web"
 )
@@ -40,4 +43,100 @@ func (exp *agendas) AgendasPage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, str)
+}
+
+// AgendaPage is the page handler for the "/agenda" path.
+func (exp *agendas) AgendaPage(w http.ResponseWriter, r *http.Request) {
+	errPageInvalidAgenda := func(err error) {
+		log.Errorf("Template execute failure: %v", err)
+		exp.server.StatusPage(w, r, web.DefaultErrorCode, "the agenda ID given seems to not exist",
+			"", web.ExpStatusNotFound)
+	}
+
+	// Attempt to get agendaid string from URL path.
+	agendaId := getAgendaIDCtx(r)
+	agendaInfo, err := exp.agendasSource.AgendaInfo(agendaId)
+	if err != nil {
+		errPageInvalidAgenda(err)
+		return
+	}
+
+	summary, err := exp.dataSource.AgendasVotesSummary(agendaId)
+	if err != nil {
+		log.Errorf("fetching Cumulative votes choices count failed: %v", err)
+	}
+
+	// Overrides the default count value with the actual vote choices count
+	// matching data displayed on "Cumulative Vote Choices" and "Vote Choices By
+	// Block" charts.
+	var totalVotes uint32
+	for index := range agendaInfo.Choices {
+		switch strings.ToLower(agendaInfo.Choices[index].ID) {
+		case "abstain":
+			agendaInfo.Choices[index].Count = summary.Abstain
+		case "yes":
+			agendaInfo.Choices[index].Count = summary.Yes
+		case "no":
+			agendaInfo.Choices[index].Count = summary.No
+		}
+		totalVotes += agendaInfo.Choices[index].Count
+	}
+
+	ruleChangeI := exp.client.Params.RuleChangeActivationInterval
+	qVotes := uint32(float64(ruleChangeI) * agendaInfo.QuorumProgress)
+
+	var timeLeft string
+	blocksLeft := summary.LockedIn - int64(exp.height)
+
+	if blocksLeft > 0 {
+		// Approximately 1 block per 5 minutes.
+		var minPerblock = 5 * time.Minute
+
+		hoursLeft := int((time.Duration(blocksLeft) * minPerblock).Hours())
+		if hoursLeft > 0 {
+			timeLeft = fmt.Sprintf("%v days %v hours", hoursLeft/24, hoursLeft%24)
+		}
+	} else {
+		blocksLeft = 0
+	}
+
+	str, err := exp.server.Templates.ExecTemplateToString("agenda", struct {
+		*web.CommonPageData
+		Ai            *AgendaTagged
+		QuorumVotes   uint32
+		RuleChangeI   uint32
+		VotingStarted int64
+		LockedIn      int64
+		BlocksLeft    int64
+		TimeRemaining string
+		TotalVotes    uint32
+	}{
+		CommonPageData: exp.server.CommonData(r),
+		Ai:             agendaInfo,
+		QuorumVotes:    qVotes,
+		RuleChangeI:    ruleChangeI,
+		VotingStarted:  summary.VotingStarted,
+		LockedIn:       summary.LockedIn,
+		BlocksLeft:     blocksLeft,
+		TimeRemaining:  timeLeft,
+		TotalVotes:     totalVotes,
+	})
+
+	if err != nil {
+		log.Errorf("Template execute failure: %v", err)
+		exp.server.StatusPage(w, r, web.DefaultErrorCode, web.DefaultErrorMessage, "", web.ExpStatusError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, str)
+}
+
+func getAgendaIDCtx(r *http.Request) string {
+	hash, ok := r.Context().Value(web.CtxAgendaId).(string)
+	if !ok {
+		log.Trace("Agendaid not set")
+		return ""
+	}
+	return hash
 }
