f561a5fef3298afec18308059f9bdc059de14f99
Amos .E <anjeas4@gmail.com>
2021-11-02 06:18:19 +0100

    Fetch treasury Page data from dcrdata API

--- treasury/handler.go
+++ treasury/handler.go
@@ -1,27 +1,35 @@
 package treasury
 
 import (
-	"context"
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"math"
 	"net/http"
 	"strconv"
-	"time"
+	"strings"
 
-	"github.com/go-chi/chi"
 	"github.com/planetdecred/pdanalytics/web"
+
+	"github.com/decred/dcrd/blockchain/stake/v4"
+	"github.com/decred/dcrd/dcrutil/v4"
+	"github.com/decred/dcrdata/exchanges/v2"
+	"github.com/decred/dcrdata/v7/db/dbtypes"
 )
 
-
-
-
-
+const (
+	defaultAddressRows int64  = 20
+	MaxTreasuryRows    int64  = 200
+	txURL              string = "https://explorer.planetdecred.org/treasury/tx"
+	balURL             string = "https://explorer.planetdecred.org/treasury/balance"
+	ellipsisHTML       string = "…"
+)
 
 // TreasuryPage is the page handler for the "/treasury" path
-func (exp *explorerUI) TreasuryPage(w http.ResponseWriter, r *http.Request) {
-	ctx := context.WithValue(r.Context(), ctxAddress, exp.pageData.HomeInfo.DevAddress)
-	r = r.WithContext(ctx)
+func (trs *Treasury) TreasuryPage(w http.ResponseWriter, r *http.Request) {
 	if queryVals := r.URL.Query(); queryVals.Get("txntype") == "" {
-		// TODO: Change default to "tspend" once there are some tspends.
 		queryVals.Set("txntype", "all")
 		r.URL.RawQuery = queryVals.Encode()
 	}
@@ -30,7 +38,7 @@ func (exp *explorerUI) TreasuryPage(w http.ResponseWriter, r *http.Request) {
 	if nParam := r.URL.Query().Get("n"); nParam != "" {
 		val, err := strconv.ParseUint(nParam, 10, 64)
 		if err != nil {
-			exp.StatusPage(w, defaultErrorCode, "invalid n value", "", ExpStatusError)
+			trs.server.StatusPage(w, r, web.DefaultErrorCode, web.DefaultErrorMessage, "", web.ExpStatusError)
 			return
 		}
 		if int64(val) > MaxTreasuryRows {
@@ -48,7 +56,7 @@ func (exp *explorerUI) TreasuryPage(w http.ResponseWriter, r *http.Request) {
 	if startParam := r.URL.Query().Get("start"); startParam != "" {
 		val, err := strconv.ParseUint(startParam, 10, 64)
 		if err != nil {
-			exp.StatusPage(w, defaultErrorCode, "invalid start value", "", ExpStatusError)
+			trs.server.StatusPage(w, r, web.DefaultErrorCode, web.DefaultErrorMessage, "", web.ExpStatusError)
 			return
 		}
 		offset = int64(val)
@@ -58,22 +66,26 @@ func (exp *explorerUI) TreasuryPage(w http.ResponseWriter, r *http.Request) {
 	txTypeStr := r.URL.Query().Get("txntype")
 	txType := parseTreasuryTransactionType(txTypeStr)
 
-	txns, err := exp.dataSource.TreasuryTxns(limitN, offset, txType)
-	if exp.timeoutErrorPage(w, err, "TreasuryTxns") {
-		return
-	} else if err != nil {
-		exp.StatusPage(w, defaultErrorCode, err.Error(), "", ExpStatusError)
+	txTemp, err := trs.TreasuryTxns(TxParams{
+		Limit:  limitN,
+		Offset: offset,
+		TxType: txType,
+	})
+
+	txns := txTemp.Txns
+
+	if err != nil {
+		trs.server.StatusPage(w, r, web.DefaultErrorCode, err.Error(), "", web.ExpStatusError)
 		return
 	}
 
-	exp.pageData.RLock()
-	treasuryBalance := exp.pageData.HomeInfo.TreasuryBalance
-	exp.pageData.RUnlock()
+	bal, _ := trs.TreasuryBalance()
+	treasuryBalance := bal.Balance
 
 	typeCount := treasuryTypeCount(treasuryBalance, txType)
 
 	treasuryData := &TreasuryInfo{
-		Net:             exp.ChainParams.Net.String(),
+		Net:             trs.server.CommonData(r).ChainParams.Net.String(),
 		MaxTxLimit:      MaxTreasuryRows,
 		Path:            r.URL.Path,
 		Limit:           limitN,
@@ -85,7 +97,7 @@ func (exp *explorerUI) TreasuryPage(w http.ResponseWriter, r *http.Request) {
 		TypeCount:       typeCount,
 	}
 
-	xcBot := exp.xcBot
+	xcBot := trs.xcBot
 	if xcBot != nil {
 		treasuryData.ConvertedBalance = xcBot.Conversion(math.Round(float64(treasuryBalance.Balance) / 1e8))
 	}
@@ -93,20 +105,20 @@ func (exp *explorerUI) TreasuryPage(w http.ResponseWriter, r *http.Request) {
 	// Execute the HTML template.
 	linkTemplate := fmt.Sprintf("/treasury?start=%%d&n=%d&txntype=%v", limitN, txType)
 	pageData := struct {
-		*CommonPageData
+		*web.CommonPageData
 		Data        *TreasuryInfo
 		FiatBalance *exchanges.Conversion
-		Pages       []pageNumber
+		Pages       []PageNumber
 	}{
-		CommonPageData: exp.commonData(r),
+		CommonPageData: trs.server.CommonData(r),
 		Data:           treasuryData,
-		FiatBalance:    exp.xcBot.Conversion(dcrutil.Amount(treasuryBalance.Balance).ToCoin()),
+		FiatBalance:    trs.xcBot.Conversion(dcrutil.Amount(treasuryBalance.Balance).ToCoin()),
 		Pages:          calcPages(int(typeCount), int(limitN), int(offset), linkTemplate),
 	}
-	str, err := exp.templates.exec("treasury", pageData)
+	str, err := trs.server.Templates.Exec("treasury", pageData)
 	if err != nil {
 		log.Errorf("Template execute failure: %v", err)
-		exp.StatusPage(w, defaultErrorCode, defaultErrorMessage, "", ExpStatusError)
+		trs.server.StatusPage(w, r, web.DefaultErrorCode, web.DefaultErrorMessage, "", web.ExpStatusError)
 		return
 	}
 
@@ -116,185 +128,153 @@ func (exp *explorerUI) TreasuryPage(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, str)
 }
 
-// AddressPage is the page handler for the "/address" path.
-func (exp *explorerUI) AddressPage(w http.ResponseWriter, r *http.Request) {
-	// AddressPageData is the data structure passed to the HTML template
-	type AddressPageData struct {
-		*CommonPageData
-		Data         *dbtypes.AddressInfo
-		CRLFDownload bool
-		FiatBalance  *exchanges.Conversion
-		Pages        []pageNumber
-	}
-
-	// Grab the URL query parameters
-	address, txnType, limitN, offsetAddrOuts, err := parseAddressParams(r)
+func (trs *Treasury) TreasuryTxns(params TxParams) (Txns, error) {
+	client := trs.client
+	txns := Txns{}
+	paramJson, err := json.Marshal(params)
 	if err != nil {
-		exp.StatusPage(w, defaultErrorCode, err.Error(), address, ExpStatusError)
-		return
+		return txns, err
 	}
 
-	// Validate the address.
-	addr, addrType, addrErr := txhelpers.AddressValidation(address, exp.ChainParams)
-	isZeroAddress := addrErr == txhelpers.AddressErrorZeroAddress
-	if addrErr != nil && !isZeroAddress {
-		var status expStatus
-		var message string
-		code := defaultErrorCode
-		switch addrErr {
-		case txhelpers.AddressErrorDecodeFailed, txhelpers.AddressErrorUnknown:
-			status = ExpStatusBadRequest
-			message = "Unexpected issue validating this address."
-		case txhelpers.AddressErrorWrongNet:
-			status = ExpStatusWrongNetwork
-			message = fmt.Sprintf("The address %v is valid on %s, not %s.",
-				addr, exp.ChainParams.Net.String(), exp.NetName)
-			code = wrongNetwork
-		default:
-			status = ExpStatusError
-			message = "Unknown error."
+	req, err := http.NewRequest("POST", txURL, bytes.NewBuffer(paramJson))
+	if err != nil {
+		return txns, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.HttpClient.Do(req)
+	if err != nil {
+		return txns, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return txns, err
+	}
+
+	err = json.Unmarshal(body, &txns)
+	if err != nil {
+		return txns, err
+	}
+
+	return txns, nil
+}
+
+func (trs *Treasury) TreasuryBalance() (Balance, error) {
+	client := trs.client
+	bal := Balance{}
+
+	req, err := http.NewRequest("GET", balURL, nil)
+	if err != nil {
+		return bal, err
+	}
+
+	resp, err := client.HttpClient.Do(req)
+	if err != nil {
+		return bal, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return bal, err
+	}
+
+	err = json.Unmarshal(body, &bal)
+	if err != nil {
+		return bal, err
+	}
+
+	return bal, nil
+}
+
+// parseTreasuryTransactionType parses a treasury transaction type from a
+// string. If the provided string is not recognized as a treasury type, the
+// special value -1, representing "all", will be returned.
+func parseTreasuryTransactionType(txnTypeStr string) (txType stake.TxType) {
+	switch strings.ToLower(txnTypeStr) {
+	case "tspend":
+		return stake.TxTypeTSpend
+	case "tadd":
+		return stake.TxTypeTAdd
+	case "treasurybase":
+		return stake.TxTypeTreasuryBase
+	}
+	return stake.TxType(-1)
+}
+
+// treasuryTypeCount returns the tx count for the type treasury tx type. The
+// special value txType = -1 specifies all types combined.
+func treasuryTypeCount(treasuryBalance *dbtypes.TreasuryBalance, txType stake.TxType) int64 {
+	typedCount := treasuryBalance.TxCount
+	switch txType {
+	case stake.TxTypeTSpend:
+		typedCount = treasuryBalance.SpendCount
+	case stake.TxTypeTAdd:
+		typedCount = treasuryBalance.AddCount
+	case stake.TxTypeTreasuryBase:
+		typedCount = treasuryBalance.TGenCount
+	}
+	return typedCount
+}
+
+// Get a set of pagination numbers, based on a set number of rows that are
+// assumed to start from page 1 at the lowest row and ascend from there.
+// For example, if there are 20 pages of 10 rows, 0 - 199, page 1 would start at
+// row 0 and go up to row 9. If the offset is between 0 and 9, the
+// pagination would return the pageNumbers  necessary to create a pagination
+// That looks like 1 2 3 4 5 6 7 8 ... 20. The pageNumber includes a link with
+// the offset inserted using Sprintf.
+func calcPages(rows, pageSize, offset int, link string) PageNumbers {
+	if pageSize == 0 {
+		return PageNumbers{}
+	}
+	nums := make(PageNumbers, 0, 11)
+	endIdx := rows / pageSize
+	if endIdx == 0 {
+		return nums
+	}
+	pages := endIdx + 1
+	currentPageIdx := offset / pageSize
+
+	if pages > 10 {
+		nums = append(nums, makePageNumber(currentPageIdx == 0, fmt.Sprintf(link, 0), "1"))
+		start := currentPageIdx - 3
+		endMiddle := start + 6
+		if start <= 1 {
+			start = 1
+			endMiddle = 7
+		} else if endMiddle >= endIdx-1 {
+			endMiddle = endIdx - 1
+			start = endMiddle - 6
+		}
+		if start > 1 {
+			nums = append(nums, makePageNumber(false, "", ellipsisHTML))
 		}
 
-		exp.StatusPage(w, code, message, address, status)
-		return
-	}
-
-	// Handle valid but unsupported address types.
-	switch addrType {
-	case txhelpers.AddressTypeP2PKH, txhelpers.AddressTypeP2SH:
-		// All good.
-	case txhelpers.AddressTypeP2PK:
-		message := "Looks like you are searching for an address of type P2PK."
-		exp.StatusPage(w, defaultErrorCode, message, address, ExpStatusP2PKAddress)
-		return
-	default:
-		message := "Unsupported address type."
-		exp.StatusPage(w, defaultErrorCode, message, address, ExpStatusNotSupported)
-		return
-	}
-
-	// Retrieve address information from the DB and/or RPC.
-	var addrData *dbtypes.AddressInfo
-	if isZeroAddress {
-		// For the zero address (e.g. DsQxuVRvS4eaJ42dhQEsCXauMWjvopWgrVg),
-		// short-circuit any queries.
-		addrData = &dbtypes.AddressInfo{
-			Address:         address,
-			Net:             exp.ChainParams.Net.String(),
-			IsDummyAddress:  true,
-			Balance:         new(dbtypes.AddressBalance),
-			UnconfirmedTxns: new(dbtypes.AddressTransactions),
+		for i := start; i <= endMiddle; i++ {
+			nums = append(nums, makePageNumber(i == currentPageIdx, fmt.Sprintf(link, i*pageSize), strconv.Itoa(i+1)))
+		}
+		if endMiddle < endIdx-1 {
+			nums = append(nums, makePageNumber(false, "", ellipsisHTML))
+		}
+		if pages > 1 {
+			nums = append(nums, makePageNumber(currentPageIdx == endIdx, fmt.Sprintf(link, endIdx*pageSize), strconv.Itoa(pages)))
 		}
 	} else {
-		addrData, err = exp.AddressListData(address, txnType, limitN, offsetAddrOuts)
-		if exp.timeoutErrorPage(w, err, "TicketsPriceByHeight") {
-			return
-		} else if err != nil {
-			exp.StatusPage(w, defaultErrorCode, err.Error(), address, ExpStatusError)
-			return
+		for i := 0; i < pages; i++ {
+			nums = append(nums, makePageNumber(i == currentPageIdx, fmt.Sprintf(link, i*pageSize), strconv.Itoa(i+1)))
 		}
 	}
 
-	// Set page parameters.
-	addrData.IsDummyAddress = isZeroAddress // may be redundant
-	addrData.Path = r.URL.Path
-
-	// If exchange monitoring is active, prepare a fiat balance conversion
-	conversion := exp.xcBot.Conversion(dcrutil.Amount(addrData.Balance.TotalUnspent).ToCoin())
-
-	// For Windows clients only, link to downloads with CRLF (\r\n) line
-	// endings.
-	UseCRLF := strings.Contains(r.UserAgent(), "Windows")
-
-	if limitN == 0 {
-		limitN = 20
-	}
-
-	linkTemplate := fmt.Sprintf("/address/%s?start=%%d&n=%d&txntype=%v", addrData.Address, limitN, txnType)
-
-	// Execute the HTML template.
-	pageData := AddressPageData{
-		CommonPageData: exp.commonData(r),
-		Data:           addrData,
-		CRLFDownload:   UseCRLF,
-		FiatBalance:    conversion,
-		Pages:          calcPages(int(addrData.TxnCount), int(limitN), int(offsetAddrOuts), linkTemplate),
-	}
-	str, err := exp.templates.exec("address", pageData)
-	if err != nil {
-		log.Errorf("Template execute failure: %v", err)
-		exp.StatusPage(w, defaultErrorCode, defaultErrorMessage, "", ExpStatusError)
-		return
-	}
-
-	log.Tracef(`"address" template HTML size: %.2f kiB (%s, %v, %d)`,
-		float64(len(str))/1024.0, address, txnType, addrData.NumTransactions)
-
-	w.Header().Set("Content-Type", "text/html")
-	w.Header().Set("Turbolinks-Location", r.URL.RequestURI())
-	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, str)
+	return nums
 }
 
-// TreasuryTable is the handler for the "/treasurytable" path.
-func (exp *explorerUI) TreasuryTable(w http.ResponseWriter, r *http.Request) {
-	// Grab the URL query parameters
-	txType, limitN, offset, err := parseTreasuryParams(r)
-	if err != nil {
-		log.Errorf("TreasuryTable request error: %v", err)
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-
-	txns, err := exp.dataSource.TreasuryTxns(limitN, offset, txType)
-	if exp.timeoutErrorPage(w, err, "TreasuryTxns") {
-		return
-	} else if err != nil {
-		exp.StatusPage(w, defaultErrorCode, err.Error(), "", ExpStatusError)
-		return
-	}
-
-	exp.pageData.RLock()
-	bal := exp.pageData.HomeInfo.TreasuryBalance
-	exp.pageData.RUnlock()
-
-	linkTemplate := "/treasury" + "?start=%d&n=" + strconv.FormatInt(limitN, 10) + "&txntype=" + fmt.Sprintf("%v", txType)
-
-	response := struct {
-		TxnCount int64        `json:"tx_count"`
-		HTML     string       `json:"html"`
-		Pages    []pageNumber `json:"pages"`
-	}{
-		TxnCount: bal.TxCount, // + addrData.ImmatureCount,
-		Pages:    calcPages(int(treasuryTypeCount(bal, txType)), int(limitN), int(offset), linkTemplate),
-	}
-
-	type txData struct {
-		Transactions []*dbtypes.TreasuryTx
-	}
-
-	response.HTML, err = exp.templates.exec("treasurytable", struct {
-		Data txData
-	}{
-		Data: txData{
-			Transactions: txns,
-		},
-	})
-	if err != nil {
-		log.Errorf("Template execute failure: %v", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError),
-			http.StatusInternalServerError)
-		return
-	}
-
-	log.Tracef(`"treasurytable" template HTML size: %.2f kiB (%v, %d)`,
-		float64(len(response.HTML))/1024.0, txType, len(txns))
-
-	w.Header().Set("Content-Type", "application/json")
-	enc := json.NewEncoder(w)
-	//enc.SetEscapeHTML(false)
-	err = enc.Encode(response)
-	if err != nil {
-		log.Debug(err)
+func makePageNumber(active bool, link, str string) PageNumber {
+	return PageNumber{
+		Active: active,
+		Link:   link,
+		Str:    str,
 	}
 }
--- treasury/treasury.go
+++ treasury/treasury.go
@@ -1,14 +1,24 @@
 package treasury
 
+import (
+	"github.com/decred/dcrdata/exchanges/v2"
+	"github.com/planetdecred/pdanalytics/web"
+)
+
 type Treasury struct {
 	server *web.Server
+	client *Client
+	xcBot  *exchanges.ExchangeBot
 }
 
-func Activate(Context ctx, webServer *web.Server) error {
-
+func Activate(webServer *web.Server, xcBot *exchanges.ExchangeBot) error {
+	client := NewClient()
 	treasury := &Treasury{
 		server: webServer,
+		client: client,
+		xcBot:  xcBot,
 	}
 
-	treasury.server.AddRoute("/proposals")
+	treasury.server.AddRoute("/treasury", web.GET, treasury.TreasuryPage)
+	return nil
 }
